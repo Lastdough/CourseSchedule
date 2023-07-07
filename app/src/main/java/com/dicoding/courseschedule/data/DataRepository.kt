07d8f526f3e6a798d9cd66fb0ac7adef5051a38f
LastDough <abdurrahman.abdur02@gmail.com>
2023-07-07 23:29:03 +0700

    Define a local database table and DAO (data access object) based on schema in app/schemas/course.json. Use QueryUtils to create a raw query.

--- app/src/main/java/com/dicoding/courseschedule/data/DataRepository.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/DataRepository.kt
@@ -2,44 +2,57 @@ package com.dicoding.courseschedule.data
 
 import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import com.dicoding.courseschedule.util.QueryType
-import com.dicoding.courseschedule.util.QueryUtil.nearestQuery
+import com.dicoding.courseschedule.util.QueryUtil
 import com.dicoding.courseschedule.util.SortType
 import com.dicoding.courseschedule.util.executeThread
+import java.util.Calendar
 
 //TODO 4 : Implement repository with appropriate dao
 class DataRepository(private val dao: CourseDao) {
 
-    fun getNearestSchedule(queryType: QueryType) : LiveData<Course?> {
-        throw NotImplementedError("needs implementation")
+    fun getNearestSchedule(queryType: QueryType): LiveData<Course?> {
+        val nearestQuery = QueryUtil.nearestQuery(queryType)
+        return dao.getNearestSchedule(nearestQuery)
     }
 
     fun getAllCourse(sortType: SortType): LiveData<PagedList<Course>> {
-        throw NotImplementedError("needs implementation")
+        val sortedQuery = QueryUtil.sortedQuery(sortType)
+        val courses = dao.getAll(sortedQuery)
+
+        val configuration = PagedList.Config.Builder()
+            .setEnablePlaceholders(PLACEHOLDER)
+            .setInitialLoadSizeHint(PAGE_SIZE)
+            .setPageSize(PAGE_SIZE)
+            .build()
+        return LivePagedListBuilder(courses, configuration).build()
     }
 
-    fun getCourse(id: Int) : LiveData<Course> {
-        throw NotImplementedError("needs implementation")
+    fun getCourse(id: Int): LiveData<Course> {
+        return dao.getCourse(id)
     }
 
-    fun getTodaySchedule() : List<Course> {
-        throw NotImplementedError("needs implementation")
+    fun getTodaySchedule(): List<Course> {
+        val calendar = Calendar.getInstance()
+        val today = calendar.get(Calendar.DAY_OF_WEEK)
+        return dao.getTodaySchedule(today)
     }
 
     fun insert(course: Course) = executeThread {
-
+        dao.insert(course)
     }
 
     fun delete(course: Course) = executeThread {
-
+        dao.delete(course)
     }
 
     companion object {
         @Volatile
         private var instance: DataRepository? = null
         private const val PAGE_SIZE = 10
+        const val PLACEHOLDER = true
 
         fun getInstance(context: Context): DataRepository? {
             return instance ?: synchronized(DataRepository::class.java) {
