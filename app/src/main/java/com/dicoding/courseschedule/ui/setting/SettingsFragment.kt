efb462e66e066d77f604319181cc2a5c31657e45
LastDough <abdurrahman.abdur02@gmail.com>
2023-07-08 21:20:35 +0700

    Update dark mode theme based on the value in ListPreference.

--- app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
@@ -2,15 +2,38 @@ package com.dicoding.courseschedule.ui.setting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.notification.DailyReminder
+import com.dicoding.courseschedule.util.NightMode
+import java.util.Locale
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    private val dailyReminder : DailyReminder = DailyReminder()
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
         //TODO 10 : Update theme based on value in ListPreference
+        val themePreferences = findPreference<ListPreference>(getString(R.string.pref_key_dark))
+        themePreferences?.setOnPreferenceChangeListener { _, newValue ->
+            val theme = NightMode.valueOf(newValue.toString().uppercase(Locale.US))
+            updateTheme(theme.value)
+            true
+        }
         //TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
+        val prefNotification = findPreference<SwitchPreference>(getString(R.string.pref_key_notify))
+        prefNotification?.setOnPreferenceChangeListener { _, newValue ->
+            val checkedStatus = newValue as Boolean
+            if (checkedStatus){
+                dailyReminder.setDailyReminder(requireContext())
+            } else {
+                dailyReminder.cancelAlarm(requireContext())
+            }
+            true
+        }
+
     }
 
     private fun updateTheme(nightMode: Int): Boolean {
