b1563a7b6cba3f1ba0d1481ce08adf7b414e1a4a
LastDough <abdurrahman.abdur02@gmail.com>
2023-07-08 17:31:22 +0700

    Initiate RecyclerView with CourseAdapter

--- app/src/main/java/com/dicoding/courseschedule/paging/CourseViewHolder.kt
+++ app/src/main/java/com/dicoding/courseschedule/paging/CourseViewHolder.kt
@@ -1,12 +1,13 @@
 package com.dicoding.courseschedule.paging
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
 import com.dicoding.courseschedule.util.DayName.Companion.getByNumber
 
-class CourseViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class CourseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private lateinit var course: Course
     private val timeString = itemView.context.resources.getString(R.string.time_format)
@@ -19,7 +20,11 @@ class CourseViewHolder(view: View): RecyclerView.ViewHolder(view) {
             val dayName = getByNumber(day)
             val timeFormat = String.format(timeString, dayName, startTime, endTime)
 
-
+            itemView.apply {
+                findViewById<TextView>(R.id.tv_course).text = courseName
+                findViewById<TextView>(R.id.tv_time).text = timeFormat
+                findViewById<TextView>(R.id.tv_lecturer).text = lecturer
+            }
         }
 
         itemView.setOnClickListener {
